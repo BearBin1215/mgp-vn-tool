ebac335293c2222c0612b6715a74af08ce28f7e8
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2026-06-25 14:38:56 +0800

    init: 仓库初始化
    - 开发、框架配置、CI脚本
    - 应用入口文件
    - 图标等静态资源

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+const host = process.env.TAURI_DEV_HOST;
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    devSourcemap: true,
+  },
+
+  build: {
+    chunkSizeWarningLimit: 4096,
+  },
+  // 防止 Vite 遮挡 Rust 错误信息
+  clearScreen: false,
+  // Tauri 要求固定端口，端口不可用时构建失败
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // Vite 忽略监听 `src-tauri` 目录
+      ignored: ['**/src-tauri/**'],
+    },
+  },
+});
