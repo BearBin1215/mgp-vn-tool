ed8657584ae4fcbc63735331be85b65ba36386aa
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2026-07-01 15:31:56 +0800

    fix(component): 修正帮助弹窗打开后只能通过Esc关闭问题

--- src/components/HelpButton.tsx
+++ src/components/HelpButton.tsx
@@ -1,5 +1,5 @@
-import type { ReactNode } from 'react';
-import { App, Button, Tooltip } from 'antd';
+import { useState, type ReactNode } from 'react';
+import { Button, Tooltip, Modal } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
 
 interface HelpButtonProps {
@@ -9,28 +9,24 @@ interface HelpButtonProps {
 
 /** 使用帮助按钮，点击后通过 App.useApp().modal 弹出统一样式的帮助弹窗 */
 export default function HelpButton({ children }: HelpButtonProps) {
-  const { modal } = App.useApp();
-
-  const handleClick = () => {
-    modal.info({
-      title: '使用帮助',
-      width: 620,
-      footer: null,
-      content: (
-        <ul className='pl-4 m-0 list-disc'>
-          {children}
-        </ul>
-      ),
-    });
-  };
+  const [helpModalOpen, setHelpModalOpen] = useState(false);
 
   return (
     <Tooltip title='使用帮助'>
       <Button
         type='text'
         icon={<QuestionCircleOutlined />}
-        onClick={handleClick}
+        onClick={() => setHelpModalOpen(true)}
       />
+      <Modal
+        open={helpModalOpen}
+        title='使用帮助'
+        footer={null}
+        onCancel={() => setHelpModalOpen(false)}
+        width={620}
+      >
+        {children}
+      </Modal>
     </Tooltip>
   );
 }
