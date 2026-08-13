ff59b620ac6f68c1d65b4060ec651f5e32419449
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2026-08-13 14:20:46 +0800

    build(deps): 更新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ command }) => ({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(import.meta.dirname, 'src'),
     },
   },
   css: {
